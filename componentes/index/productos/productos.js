33c15464ae8e6e0c9b7d9bdda4ca741905c66c31
Lucas Sanchez <lucasgex11@gmail.com>
2023-06-30 20:41:11 -0300

    mas mejoras

--- componentes/index/productos/productos.js
+++ componentes/index/productos/productos.js
@@ -1,71 +1,4 @@
-/*const productos = document.querySelectorAll(".productos__seccion__contenedor");
-const verTodo = document.querySelector(".ver__todo");
 
-let mostrar = false;
-
-function mostrarTodos() {
-  productos.forEach((producto) => {
-    if (mostrar) {
-      producto.style.display = "none";
-    } else {
-      producto.style.display = "block";
-    }
-  });
-
-  mostrar = !mostrar; // Alternar el estado
-}
-
-verTodo.onclick = mostrarTodos;*/
-
-
-
-
-/*verTodoEnlace.addEventListener('click', () => {
-  mostrarTodoStar = false;
-  mostrarTodoConsolas=false;
-  mostrarTodoDiversos=false;
-
-  if (mostrarTodoStar ) {
-    productosStar.forEach(producto => {
-      producto.style.display = 'block';
-    });
-    verTodoEnlace.innerHTML = '<p>Ver menos <i class="fa-solid fa-arrow-left" style="color: #2a7ae4;"></i></p>';
-  }
-  else if (mostrarTodoConsolas ) {
-    productosConsolas.forEach(producto => {
-      producto.style.display = 'block';
-    });
-    verTodoEnlace.innerHTML = '<p>Ver menos <i class="fa-solid fa-arrow-left" style="color: #2a7ae4;"></i></p>';
-  }
-  else if (mostrarTodoDiversos ) {
-    productosDiversos.forEach(producto => {
-      producto.style.display = 'block';
-    });
-    verTodoEnlace.innerHTML = '<p>Ver menos <i class="fa-solid fa-arrow-left" style="color: #2a7ae4;"></i></p>';
-  } else {
-    if (window.matchMedia('(min-width: 1140px)').matches) {
-      productosContenedores.forEach((producto, index) => {
-        if (index >= 6) {
-          producto.style.display = 'none';
-        }
-      });
-    } else if (window.matchMedia('(min-width: 768px)').matches) {
-      productosproductoes.forEach((producto, index) => {
-        if (index >= 4) {
-          producto.style.display = 'none';
-        }
-      });
-    } else {
-      productosproductoes.forEach((producto, index) => {
-        if (index >= 2) {
-          producto.style.display = 'none';
-        }
-      });
-    }
-
-    verTodoEnlace.innerHTML = '<p>Ver todo <i class="fa-solid fa-arrow-right" style="color: #2a7ae4;"></i></p>';
-  }
-});*/
 
 const verTodoStar = document.querySelector('.vertodo__star');
 const verTodoConsolas = document.querySelector('.vertodo__consolas');
@@ -76,6 +9,8 @@ const productosConsolas = document.querySelectorAll('.producto__consolas');
 const productosDiversos = document.querySelectorAll('.producto__diversos');
 
 let mostrarTodoStar = false;
+let mostrarTodoConsolas = false;
+let mostrarTodoDiversos =false;
 
 verTodoStar.addEventListener("click",()=>{
    mostrarTodoStar = !mostrarTodoStar
@@ -110,6 +45,69 @@ verTodoStar.addEventListener("click",()=>{
   }
 })
 
+verTodoConsolas.addEventListener("click",()=>{
+  mostrarTodoConsolas = !mostrarTodoConsolas
 
-console.log(productosStar)
+ if(mostrarTodoConsolas){
+   productosConsolas.forEach( producto =>{
+     producto.style.display = "block";
+   })
+   verTodoConsolas.innerHTML = '<p>Mostrar menos <i class="fa-solid fa-arrow-left" style="color: #2a7ae4;"></i></p>';
+ } else {
+   if (window.matchMedia('(min-width: 1140px)').matches) {
+     productosConsolas.forEach((producto, index) => {
+       if (index >= 6) {
+         producto.style.display = 'none';
+       }
+     });
+   } else if (window.matchMedia('(min-width: 768px)').matches) {
+     productosConsolas.forEach((producto, index) => {
+       if (index >= 4) {
+         producto.style.display = 'none';
+       }
+     });
+   } else {
+     productosConsolas.forEach((producto, index) => {
+       if (index >= 2) {
+         producto.style.display = 'none';
+       }
+     });
+   }
+
+   verTodoConsolas.innerHTML = '<p>Ver todo <i class="fa-solid fa-arrow-right" style="color: #2a7ae4;"></i></p>';
+ }
+})
+
+verTodoDiversos.addEventListener("click",()=>{
+  mostrarTodoDiversos = !mostrarTodoDiversos
+
+ if(mostrarTodoDiversos){
+   productosDiversos.forEach( producto =>{
+     producto.style.display = "block";
+   })
+   verTodoDiversos.innerHTML = '<p>Mostrar menos <i class="fa-solid fa-arrow-left" style="color: #2a7ae4;"></i></p>';
+ } else {
+   if (window.matchMedia('(min-width: 1140px)').matches) {
+     productosDiversos.forEach((producto, index) => {
+       if (index >= 6) {
+         producto.style.display = 'none';
+       }
+     });
+   } else if (window.matchMedia('(min-width: 768px)').matches) {
+     productosDiversos.forEach((producto, index) => {
+       if (index >= 4) {
+         producto.style.display = 'none';
+       }
+     });
+   } else {
+     productosDiversos.forEach((producto, index) => {
+       if (index >= 2) {
+         producto.style.display = 'none';
+       }
+     });
+   }
+
+   verTodoDiversos.innerHTML = '<p>Ver todo <i class="fa-solid fa-arrow-right" style="color: #2a7ae4;"></i></p>';
+ }
+})
 
